9d48cd14e1be60052aa990c873eadbd9439e28fb
genjy1 <idontknower1997@gmail.com>
2023-01-08 18:12:27 +0300

    feat:added modal window for callbacks

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const wrapperCall = document.querySelector('.wrapper-call');
+const callback = document.querySelector('.callback');
+const close = document.querySelector('.heading-close');
+
+wrapperCall.addEventListener('click',()=>{
+    callback.classList.toggle('modal-active')
+})
+
+close.addEventListener('click',()=>{
+    callback.classList.toggle('modal-active')
+})
