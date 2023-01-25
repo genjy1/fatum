cc43d79beb0c84afdb2f55a0e251127466e87b46
genjy1 <idontknower1997@gmail.com>
2023-01-25 23:38:21 +0300

    feat:added burger menu

--- js/cart.js
+++ js/cart.js
@@ -15,9 +15,45 @@ window.addEventListener('click',function (event) {
             imgSrc: card.querySelector('.card-img').getAttribute('src'),
         };
 
-
-    
-        const favItemHTML = `<div class="horizontal__card col-card fav__card">
+        if (window.screen.width < 426) {
+            const favItemHTML = `<div class="vertical__card">
+            <img src="${prodInf.imgSrc}" alt="" class="img vertical-img card-img">
+                <div class="vertical__cards-text__wrapper cards-text__wrapper">
+                    <p class="card-id">ID</p>
+                    <p class="card-text">Text</p>
+                    <div class="wrapper__description">
+                        <div class="desc-text__wrapper">
+                            <p class="card-district">Район</p>
+                            
+                        </div>
+                        <div class="desc-text__wrapper">
+                            <p class="card-rooms">Комнат</p>
+                            
+                        </div>
+                        <div class="desc-text__wrapper">
+                            <p class="card-square">Площадь</p>
+                            
+                        </div>
+                        <div class="desc-text__wrapper">
+                            <p class="card-toairport">До аэропорта</p>
+                            <p class="card-toairport">м</p>
+                        </div>
+                        <div class="desc-text__wrapper">
+                            <p class="card-tosea">До моря</p>
+                            <p class="card-tosea">м</p>
+                        </div>
+                    </div>
+                    <div class="inf__wrapper">
+                        <p class="card-cost" style="display: none;">Цена: от $000.000</p>
+                        <a href="#" class="learnmore link">Узнать подробнее</a>
+                        <a href="#" class="heart" data-fav></a>
+                    </div>
+                </div>
+        </div>
+        `;
+        favWrapper.insertAdjacentHTML('beforeend',favItemHTML);
+        }else{
+            const favItemHTML = `<div class="horizontal__card col-card fav__card">
         <img src="${prodInf.imgSrc}" alt="" class="img horizontal-img card-img">
         <div class="cards-text__wrapper">
             <p class="card-id"></p>
@@ -54,6 +90,9 @@ window.addEventListener('click',function (event) {
         `;
 
         favWrapper.insertAdjacentHTML('beforeend',favItemHTML);
+        }
+    
+        
     };
     if (event.target.dataset.action === 'minus') {
         if (event.target.closest('.fav__wrapper')) {
--- js/script.js
+++ js/script.js
@@ -6,10 +6,16 @@ const close = document.querySelector('.heading-close');
 const h2 = document.querySelectorAll('h2');
 const verticalCards = document.querySelectorAll('.vertical__card');
 const horizontalCards = document.querySelectorAll('.horizontal__card');
+const burger = document.querySelector('.burger');
 
 h2.forEach(e => e.classList.add('heading'));
 wrapperCall.addEventListener('click',()=>{
     callback.classList.toggle('modal-active')
+});
+
+burger.addEventListener('click', () =>{
+    const burgerMenu = document.querySelector('.burger__menu')
+    burgerMenu.classList.toggle('active')
 });
 
 verticalCards.forEach(e => e.classList.add('card'));
