6a27ad3b9da5bf0f2b1d2c1c913c3a359bc98b13
genjy1 <idontknower1997@gmail.com>
2023-02-11 14:53:56 +0300

    feat:added links

--- js/filter.js
+++ js/filter.js
@@ -41,13 +41,13 @@ for (let i = 0; i < cardId.length; i++) {
 if (city === 'cyprus') {
     document.querySelectorAll('.vertical__card').forEach(e =>{
         if (e.dataset.city !== 'cyprus'){
-            e.style.display = 'none'
+            e.remove();
         }
     });
 }else if (city === 'alanya'){
     document.querySelectorAll('.vertical__card').forEach(e =>{
         if (e.dataset.city !== 'alanya'){
-            e.style.display = 'none'
+            e.remove();
         }
     });
 }
