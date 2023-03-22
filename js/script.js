9827cbce949387e02fb18b5c1e529ff7656edae3
genjy1 <idontknower1997@gmail.com>
2023-03-22 23:55:15 +0300

    feat:filter changes

--- js/script.js
+++ js/script.js
@@ -15,17 +15,29 @@ const moreFilters = document.querySelector('.more-filters');
 
 moreFilters.addEventListener('click', e =>{
     e.preventDefault();
+    e.target.textContent = 'Скрыть фильтры';
+    e.target.classList.toggle('extended');
     const moreFiltersLabel = document.querySelector('.more-filters__label');
     moreFiltersLabel.classList.toggle('active__filter');
 })
 
-// const cardsFiltered = [];
+moreFilters.addEventListener('click',e => {
+    if (e.target.classList.contains('extended') == false) {
+        e.target.textContent = 'Больше фильтров';
+    }
+})
+
+const filterOptCity = document.querySelector('#citySelect');
+filterOptCity.addEventListener('click', e =>{
+    console.log(filterOpt.value);
+    const filterLink = document.querySelector('.search-submit');
+    filterLink.setAttribute('href',`../pages/filter.html?=${filterOptCity.value}`)
+});
 const filterOpt = document.querySelector('#districtSelect');
 filterOpt.addEventListener('click', e =>{
     console.log(filterOpt.value);
     const filterLink = document.querySelector('.search-submit');
     filterLink.setAttribute('href',`../pages/filter.html?=${filterOpt.value}`)
-    console.log(filterLink);
 });
 
 heart.forEach(e => e.removeAttribute('href'));
